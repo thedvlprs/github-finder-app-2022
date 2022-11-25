810c18bd0bc5be05b00b025f8d1eadca2c701ca6
henrichard <glosskraken@gmail.com>
2022-11-25 12:08:50 +0500

    feat: UserList component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import UserResults from '../components/users/UserResults';
 function Home() {
     return (
-        <div>
-            <h1 className='text-6xl'>Welcome</h1>
-            {import.meta.env.VITE_GITHUB_URL}
-        </div>
+        <>
+            {/* SEARCH COMPONENT GO HERE */}
+            <UserResults />
+        </>
     );
 }
 
