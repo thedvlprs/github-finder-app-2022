9ef9cb19f5c463a029b89bb38883b7c7ef7f7cc8
henrichard <glosskraken@gmail.com>
2022-11-25 16:08:27 +0500

    feat: setup github context

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,28 @@ import About from './pages/About';
 import NotFound from './pages/NotFound';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
+import { GithubProvider } from './context/github/GithubContext';
 
 import './index.css';
 
 function App() {
     return (
-        <Router>
-            <div className='flex flex-col justify-between h-screen'>
-                <Navbar />
-                <main className='container mx-auto px-3 pb-12'>
-                    <Routes>
-                        <Route path='/' element={<Home />} />
-                        <Route path='/about' element={<About />} />
-                        <Route path='/notfound' element={<NotFound />} />
-                        <Route path='/*' element={<NotFound />} />
-                    </Routes>
-                </main>
-                <Footer />
-            </div>
-        </Router>
+        <GithubProvider>
+            <Router>
+                <div className='flex flex-col justify-between h-screen'>
+                    <Navbar />
+                    <main className='container mx-auto px-3 pb-12'>
+                        <Routes>
+                            <Route path='/' element={<Home />} />
+                            <Route path='/about' element={<About />} />
+                            <Route path='/notfound' element={<NotFound />} />
+                            <Route path='/*' element={<NotFound />} />
+                        </Routes>
+                    </main>
+                    <Footer />
+                </div>
+            </Router>
+        </GithubProvider>
     );
 }
 
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,27 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import UserItem from '../users/UserItem';
-
+import GithubContext from '../../context/github/GithubContext';
 function UserResults() {
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const { users, loading, fetchUsers } = useContext(GithubContext);
 
     useEffect(() => {
         fetchUsers();
     }, []);
-
-    const fetchUsers = async () => {
-        const res = await fetch(`${import.meta.env.VITE_GITHUB_URL}/users`, {
-            headers: {
-                // Authorization: `token ${import.meta.env.VITE_GITHUB_TOKEN}`,
-            },
-        });
-
-        const data = await res.json();
-
-        setUsers(data);
-        setLoading(false);
-    };
 
     if (!loading) {
         return (
