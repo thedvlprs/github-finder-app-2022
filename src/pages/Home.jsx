5bd04cc298e96957c4b1f91609ebebc27dd6fa61
henrichard <glosskraken@gmail.com>
2022-11-28 12:01:45 +0500

    feat: user search component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import UserResults from '../components/users/UserResults';
+import UserSearch from '../components/users/UserSearch';
 function Home() {
     return (
         <>
-            {/* SEARCH COMPONENT GO HERE */}
+            <UserSearch />
             <UserResults />
         </>
     );
