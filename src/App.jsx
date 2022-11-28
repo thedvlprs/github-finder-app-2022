5a42f51dac697ee15c3b306b15ea0652de73782f
henrichard <glosskraken@gmail.com>
2022-11-28 15:32:37 +0500

    feat: add alert component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
 import Footer from './components/layout/Footer';
+import Alert from './components/layout/Alert';
 import Navbar from './components/layout/Navbar';
 import { GithubProvider } from './context/github/GithubContext';
 import { AlertProvider } from './context/alert/AlertContext';
@@ -19,6 +20,7 @@ function App() {
                     <div className='flex flex-col justify-between h-screen'>
                         <Navbar />
                         <main className='container mx-auto px-3 pb-12'>
+                            <Alert />
                             <Routes>
                                 <Route path='/' element={<Home />} />
                                 <Route path='/about' element={<About />} />
